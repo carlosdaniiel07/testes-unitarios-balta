2313e13e1e54bfab89008d3cc8e5fac8a0177ab9
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-07-22 23:15:33 -0300

    Add CreateOrderCommandTests

---
+++ good/Store/Store.Domain/Commands/CreateOrderItemCommand.cs
@@ -0,0 +1,33 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Interfaces;
+using System;   
+
+namespace Store.Domain.Commands
+{
+    public class CreateOrderItemCommand : Notifiable, ICommand
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        public CreateOrderItemCommand()
+        {
+
+        }
+
+        public CreateOrderItemCommand(Guid productId, int quantity)
+        {
+            ProductId = productId;
+            Quantity = quantity;
+        }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasLen(ProductId.ToString(), 32, "Product", "Produto inválido")
+                .IsGreaterThan(Quantity, 0, "Quantity", "Quantidade inválida")
+            );
+        }
+    }
+}
---
+++ good/Store/Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using Store.Domain.Commands;
+using System;
+using Xunit;
+
+namespace Store.Tests.Commands
+{
+    public class CreateOrderCommandTests
+    {
+        [Fact]
+        public void Dado_um_comando_invalido_o_pedido_nao_deve_ser_gerado()
+        {
+            var command = new CreateOrderCommand
+            {
+                Customer = string.Empty,
+                ZipCode = string.Empty,
+                PromoCode = null
+            };
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+
+            command.Validate();
+
+            Assert.True(command.Invalid);
+        }
+    }
+}
